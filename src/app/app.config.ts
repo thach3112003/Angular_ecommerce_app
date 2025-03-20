bba1da6353759f3debb6138eea1c19af0939143e
thach3112003 <thach3112003@gmail.com>
2025-03-20 16:13:51 +0700

    Test

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, NgModule, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { FormsModule } from '@angular/forms';
 
+@NgModule({
+  imports: [
+    FormsModule, // Thêm dòng này
+  ],
+})
+export class AppModule {}
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes)
+  ]
 };
