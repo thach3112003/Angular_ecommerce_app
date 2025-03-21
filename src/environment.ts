6b144c4dba35622b2143bda169927777c46752ff
thach3112003 <thach3112003@gmail.com>
2025-03-21 21:11:05 +0700

    Update Nav, footer

---
+++ src/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCv2sL3VCyT1-G6NvmgFk_kSH2UPLY2bZE",
+  authDomain: "angular-ecommerce-projec-ac115.firebaseapp.com",
+  projectId: "angular-ecommerce-projec-ac115",
+  storageBucket: "angular-ecommerce-projec-ac115.firebasestorage.app",
+  messagingSenderId: "768983763464",
+  appId: "1:768983763464:web:d247e4ad8b6472502a9fbd",
+  measurementId: "G-J0423675X5"
+  }
+};
